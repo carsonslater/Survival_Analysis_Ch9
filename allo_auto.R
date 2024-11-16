19ab9bd491f7ed0d6983fcbb1f1a3bd818e764a0
Carson Slater <carsonslater7@gmail.com>
2024-11-15 18:01:21 -0600

    adding plots to notes pdf

--- allo_auto.R
+++ allo_auto.R
@@ -6,14 +6,16 @@
 library("tidyverse")
 library("KMsurv")
 library("survival")
+library("scales", warn.conflicts = FALSE) 
+library("patchwork")
+library("tinytex")
+theme_update(panel.grid.minor = element_blank())
+
+baylor_green <- "#154734"
+baylor_gold <- "#ffb81c"
 
 aa <- data("alloauto")
 
-# Recreate Figure 9.1 -----------------------------------------------------
-
-# Assume there is a grouping variable (e.g., alloauto$group)
-# If no grouping variable exists, replace 'group' with the appropriate column name in your dataset.
-# Example uses 'group' as a placeholder; adjust if necessary.
 
 # Fit the survival model for each group and extract cumulative hazards
 km_fit_groups <- alloauto |> 
@@ -38,44 +40,74 @@ auto_fit <- alloauto %>%
 
 tb <- tibble(
   "Time" = 1:length(auto_fit$cumhaz[-49]),
-  "allo_ch" = c(allo_fit$cumhaz, NA),
-  "auto_ch" = auto_fit$cumhaz[-49]
+  "Allo" = c(allo_fit$cumhaz, NA),
+  "Auto" = auto_fit$cumhaz[-49]
 ) |> pivot_longer(
-  cols = c("allo_ch", "auto_ch"),
+  cols = c("Allo", "Auto"),
   names_to = "type"
-)
+) |> mutate(
+  l_time = log(Time),
+  l_val = log(value),
+  l_norm = qnorm(1 - exp(-value)),
+  l_logistic = log(exp(value - 1))
+) 
 
-tb |> 
+# 1st Plot Attempt --------------------------------------------------------
+# Figure 12.1
+
+p1 <- tb |> 
   ggplot(aes(Time, value, colour = type)) +
-  geom_step()
+  geom_step() +
+  scale_color_manual(values = c(baylor_gold, baylor_green)) +
+  labs(title = "Exponential",
+       x = "Time",
+       y = "Est. Cumulative Hazard",
+       color = "Group") +
+  theme_minimal(base_size = 10) +
+  theme(legend.position = "none")
 
 
-# Plot the log cumulative hazard for each group
-# ggplot(km_fit_groups, aes(x = time, y = log(cumhaz), group = type, color = type)) +
-#   geom_step() +
-#   labs(x = "Time", y = "Log Cumulative Hazard", 
-#        title = "Log Estimated Cumulative Hazard by Group") +
-#   xlim(0, 30) +
-#   theme_minimal()
+# Figure 12.2 -------------------------------------------------------------
 
-# 
-# # Fit a Kaplan-Meier survival curve
-# km_fit <- survfit(Surv(time, event = delta) ~ type, data = alloauto)
-# 
-# plot(km_fit)
-# 
-# # Convert survival function to cumulative hazard
-# cumulative_hazard <- log(km_fit$cumhaz)
-# 
-# # Create a data frame for plotting
-# cumhaz_data <- data.frame(
-#   time = km_fit$time,
-#   cumulative_hazard = cumulative_hazard
-# )
-# 
-# # Plot the cumulative hazard function
-# ggplot(cumhaz_data, aes(x = time, y = cumulative_hazard)) +
-#   geom_step() +
-#   labs(x = "Time", y = "Cumulative Hazard", title = "Estimated Cumulative Hazard Function") +
-#   theme_minimal()
-# 
+
+p2 <- tb |> 
+  ggplot(aes(l_time, l_val, colour = type)) +
+  geom_step() +
+  scale_color_manual(values = c(baylor_gold, baylor_green)) +
+  labs(title = "Weibull",
+       x = "Log Time",
+       y = "Log Est. Cumulative Hazard",
+       color = "Group")  +
+  theme_minimal(base_size = 10)
+
+
+
+# Figure 12.3 -------------------------------------------------------------
+
+p3 <- tb |> 
+  ggplot(aes(l_time, l_norm, colour = type)) +
+  geom_step() +
+  scale_color_manual(values = c(baylor_gold, baylor_green)) +
+  labs(title = "Log-Normal",
+       x = "Log Time",
+       y = "Probit(1 - exp(H(t)))",
+       color = "Group") +
+  theme_minimal(base_size = 10) +
+  theme(legend.position = "none")
+
+
+# Figure 12.4 -------------------------------------------------------------
+
+p4 <- tb |> 
+  ggplot(aes(l_time, l_logistic, colour = type)) +
+  geom_step() +
+  scale_color_manual(values = c(baylor_gold, baylor_green)) +
+  labs(title = "Log-Logistic",
+       x = "Log Time",
+       y = "Log Odds",
+       color = "Group") +
+  theme_minimal(base_size = 10) +
+  theme(legend.position = "none")
+
+
+(p1 + p2) / (p3 + p4)
